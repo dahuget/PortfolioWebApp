33b14f2c2aafaa10822afe2f2c19d61c0e9bed8f
dahuget <danaliyahuget@gmail.com>
2023-10-26 12:21:17 -0700

    Firebase code update

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAqwkiUakt2JyIL-gpZbfsTWiB1j-Ki3jQ",
   authDomain: "danahuget-portfolio.firebaseapp.com",
@@ -14,5 +10,4 @@ const firebaseConfig = {
   appId: "1:655414612483:web:df1b6609c5dcca41"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
